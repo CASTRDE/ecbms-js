cf1637d6b73a7af31303bf5fadc21690c10970fb
Desiree Castro <desiree.anne.castro@oocl.com>
2022-11-07 17:44:09 +0800

    [feat] route setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
-// import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import SiteLayout from "./layout/SiteLayout";
+import LoginPage from "./pages/LoginPage";
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
     <div className="App">
-      <SiteLayout/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SiteLayout />}>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

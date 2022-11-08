a059f3200c7a70cb4bb51acc3bee794456eb4488
Desiree Castro <desiree.anne.castro@oocl.com>
2022-11-08 10:15:27 +0800

    [feat] log in page with foreground

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,20 @@
+import { Col, Row } from "antd";
 import React from 'react';
-import "./LoginPage.css"
+import "./LoginPage.css";
 
 function LoginPage() {
 
     return (
-        <div className="login-page-bg">
-            
+        <>
+        <div className="login-page-bg-container" />
+        <div className="foreground">
+            <Row>
+                <Col className="foreground-logo" span={12}>logo</Col>
+                <Col className="foreground-login"span={12}>login</Col>
+            </Row>
         </div>
+        
+        </>
      );
 }
 

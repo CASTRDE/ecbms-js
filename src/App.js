c29df082ccb6499227799dc51ce45ad2e6da77e9
dumoljh <dumoljh@oocl.com>
2022-11-09 09:05:55 +0800

    feat: [ST010003] finish side menu, uncomplete routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
-// import { BrowserRouter, Routes, Route} from "react-router-dom";
-import SiteLayout from "./layout/SiteLayout";
+import SideBar from "./components/SideBar"
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import SearchEntry from "./pages/SearchEntry";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <SiteLayout/>
-    </div>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element = {<SideBar />} >
+                <Route path="/NewLodgement" element={<div>New Lodgement</div>} />
+                <Route path="/SearchEntry" element={<SearchEntry />} />
+                <Route path="/Documents" element={<div>Documents</div>} />
+            </Route>
+        </Routes>
+      </BrowserRouter>
   );
 }
 

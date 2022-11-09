c5755496bfe9cb5b902df83557c5ab34d41b1201
Desiree Castro <desiree.anne.castro@oocl.com>
2022-11-09 11:09:23 +0800

    [feat] create page setup

--- src/pages/CreatePasswordPage.jsx
+++ src/pages/CreatePasswordPage.jsx
@@ -1,10 +1,56 @@
+import { Form, Input, Button } from 'antd';
 import React from 'react';
-import { Link } from "react-router-dom";
 import "./CreatePasswordPage.css";
 
 function CreatePasswordPage() {
     return(
-        <>Sample</>
+        <>
+        <div className="form-container">
+            <Form
+                layout='vertical'>
+                <p className='form-header'>Create New Password</p>
+                <Form.Item
+                    name="new-password"
+                    label="New Password"
+                    rules={[
+                        {
+                            required: true,
+                            message: ''
+                        }
+                    ]}
+                >
+                    <Input.Password
+                            // value={password}
+                            // onChange={handlePWChange}
+                            ></Input.Password>
+                </Form.Item>
+                <Form.Item
+                    name="confirm-password"
+                    label="Confirm Password"
+                    rules={[
+                        {
+                            required: true,
+                            message: ''
+                        }
+                    ]}
+                >
+                    <Input.Password
+                            // value={password}
+                            // onChange={handlePWChange}
+                            ></Input.Password>
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary"
+                        style={
+                            {   width: 69,
+                                height: 40}
+                        }
+                        // onClick={onFinish}
+                    >Save</Button>
+                </Form.Item>
+            </Form>
+        </div>
+        </>
     );
 }
 

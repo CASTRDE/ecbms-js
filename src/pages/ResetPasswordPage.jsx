61c1b28d5e6ef0be7de729e22b68c10c5283fcf9
Desiree Castro <desiree.anne.castro@oocl.com>
2022-11-09 09:47:33 +0800

    [feat] add ResetPasswordPage

---
+++ src/pages/ResetPasswordPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./ResetPasswordPage.css";
+
+function ResetPasswordPage() {
+    return (
+        <>Sample
+        </>
+    );
+}
+
+export default ResetPasswordPage;
